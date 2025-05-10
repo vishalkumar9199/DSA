286ab6f801a477f01d2d79238bf4de0f3065c8de
vishal <128027462+vishalkumar9199@users.noreply.github.com>
2025-05-11 02:40:58 +0530

    Update Hashmap.java

--- Hashmap.java
+++ Hashmap.java
@@ -1,59 +1,34 @@
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.TreeSet;
-import java.util.TreeMap;
-import java.util.HashSet;
-public class hash {
+import java.util.*;
+
+public class hashMap {
 
 	public static void main(String[] args) {
 	
-		//HashMap... put,containKey,delete,Size();
-          HashMap<String,Integer> lhm = new HashMap<>();
+	//HashMap... put,containKey,delete,Size(); unorder 
+          HashMap<String,Integer> map = new HashMap<>();
+       
+		//PUT FUNCTION
+          map.put("India",100);
+          map.put("New Zealand", 20);
+          map.put("America", 120);
+          map.get("India");
           
-          //LinkedHashMap
           
-         // LinkedHashMap<String,Integer> lhm= new LinkedHashMap<>();
           
-          //TreeMap.. already sorted
-           //TreeMap<String,Integer>tmp= new TreeMap<>();
+          for(Map.Entry<String,Integer>e:map.entrySet()) {
+        	  System.out.println(e.getKey());
+        	  System.out.println(e.getValue());
+          }
 		
 		   
-               //PUT FUNCTION
-              lhm.put("India",100);
-              lhm.put("New Zealand", 20);
-               lhm.put("America", 120);
-               lhm.get("India");
-               System.out.println( lhm.get("India"));
-               System.out.println(lhm);
+               
+              System.out.println( map.get("India"));
+              System.out.println(map);
+               
+               
         
        
-           
-		
-		
-		// HashSet => contain unique element...add,containkey,set.size(),remove;
-				
-		HashSet<String> hs= new HashSet<>();
-		LinkedHashSet<String> lhs= new LinkedHashSet<>();
-		TreeSet<String>ts= new TreeSet<>();
-		lhs.add("vishal");
-		lhs.add("Peter");
-		lhs.add("Rosers");
-		lhs.add("Tony");
-		
-		Iterator it= hs.iterator();
-		while(it.hasNext()) {
-			System.out.println(it.next());
-		}
-		
-		for(String name:lhs) {
-			System.out.println(name);
-		}
-		
-       
-         
-         
+          
          
 	}
 }
