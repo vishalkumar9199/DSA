f99d6df1b98279348fbd950f93d54a378e6752aa
vishal <128027462+vishalkumar9199@users.noreply.github.com>
2025-05-16 02:51:51 +0530

    Update String.java

--- String.java
+++ String.java
@@ -74,8 +74,31 @@ public class basic {
             System.out.println(asciivalue);
         }
 
-    
-				
+		// Toggle the word eg mAtH => MaTh
+import java.util.*;
+class Main {
+  
+    public static void main(String[] args) {
+        StringBuilder str= new StringBuilder("ViShaL");
+        for(int i=0;i<str.length();i++){
+            boolean flag=true;//capital
+            char ch=str.charAt(i);
+            int ascii=(int)ch;
+            if(ascii>=97) flag=false;//small
+            if(flag==true){
+                ascii +=32;
+                char dh = (char)ascii;
+                str.setCharAt(i,dh);
+            }else{
+                ascii -=32;
+                char dh=(char)ascii;
+                str.setCharAt(i,dh);
+            }
+        }
+        System.out.println(str);
+       
+    }
+}
 
 	}
 
