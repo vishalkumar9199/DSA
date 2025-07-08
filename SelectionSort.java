afed810566d3b2ffaa197a86b414519e21f20b50
vishal <128027462+vishalkumar9199@users.noreply.github.com>
2025-07-08 21:53:30 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,6 @@
 // selection sort=>compare and swap
-     public static void selectionsort(int[]arr) {
+public class q1{
+  public static void selectionsort(int[]arr) {
     	 for(int i=0;i<arr.length;i++) {
     		 int minIndex=i;
     		 for(int j=i+1;j<arr.length;j++) {
@@ -16,3 +17,11 @@
     	     }
     	 }
      }
+  public static void main(String[] args) {
+		
+		int[]myarray= {5,9,4,8,1,3,2,6,7,99};
+		System.out.println("The unsorted array is:"+Arrays.toString(myarray));
+		selectionsort(myarray);
+		System.out.println("The sorted array is:"+Arrays.toString(myarray));
+  }
+}
